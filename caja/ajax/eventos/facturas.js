ee34de557307e23c6e4880cf08c55655a6615f4f
Hector Esparza <hesparza@hulesapa.mx>
2019-04-09 09:48:50 -0500

    Añadiendo archivo para comprobar si el archivo de cargas es excel

---
+++ caja/ajax/eventos/facturas.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $("#archivo").change(function(){
+    var archivo = $("#archivo").val();
+    arreglo = archivo.split("\\");
+    arreglo = arreglo[arreglo.length-1];
+    //alert("Introduciste el archivo "+arreglo+" y el tamaño es de "+arreglo.length);
+    arreglo = arreglo.split(".");
+    arreglo = arreglo[1];
+    if (arreglo=="xlsx"||arreglo=="xls"){
+      $("#cargar").prop("disabled", false);
+    }
+    else {
+      alert("Introduce un documento válido, solamente se permiten archivos con extensión .xlsx "+arreglo);
+      $("#cargar").prop("disabled", true);
+    }
+  });
+
+});
