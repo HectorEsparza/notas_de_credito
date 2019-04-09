0a027c90082c0d7e3fb59221efb4d0f9a632d5f7
Hector Esparza <hesparza@hulesapa.mx>
2019-04-09 10:05:50 -0500

    Guarda las facturas rescatadas del archivo excel dentro de la base de datos

---
+++ caja/ajax/eventos/guardaFacturas.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+  $("#guardar").click(function(){
+    //alert("Se va a guardar las facturas");
+    enviar();
+  });
+  function enviar(){
+
+    var factura = [];
+    var cliente = [];
+    var nombre = [];
+    var estatus = [];
+    var fecha = [];
+    var descuento = [];
+    var importe = [];
+    var vendedor = [];
+    var contador = $("#contador").val();
+    for (var i = 1; i <= contador; i++){
+      factura[i] = $("#factura"+i).text();
+      cliente[i] = $("#cliente"+i).text();
+      nombre[i] = $("#nombre"+i).text();
+      estatus[i] = $("#estatus"+i).text();
+      fecha[i] = $("#fecha"+i).text();
+      descuento[i] = $("#descuento"+i).text();
+      importe[i] = $("#importe"+i).text();
+      vendedor[i] = $("#vendedor"+i).text()
+    }
+    var parametros =
+    {
+      factura: factura,
+      cliente: cliente,
+      nombre: nombre,
+      estatus: estatus,
+      fecha: fecha,
+      descuento: descuento,
+      importe: importe,
+      vendedor: vendedor,
+      contador: contador,
+    }
+    $.ajax({
+        async: true, //Activar la transferencia asincronica
+        type: "POST", //El tipo de transaccion para los datos
+        dataType: "json", //Especificaremos que datos vamos a enviar
+        contentType: "application/x-www-form-urlencoded", //Especificaremos el tipo de contenido
+        url: "ajax/guardaFacturas.php", //Sera el archivo que va a procesar la petición AJAX
+        data: parametros, //Datos que le vamos a enviar
+        // data: "total="+total+"&penalizacion="+penalizacion,
+        beforeSend: inicioEnvio, //Es la función que se ejecuta antes de empezar la transacción
+        success: llegada, //Función que se ejecuta en caso de tener exito
+        timeout: 4000,
+        error: problemas //Función que se ejecuta si se tiene problemas al superar el timeout
+    });
+    return false;
+  }
+  function inicioEnvio(){
+    console.log("Cargando...");
+  }
+
+  function llegada(datos){
+    alert(datos[0]);
+    setTimeout("location.href='visualizacion.php'",500);
+  }
+
+  function problemas(){
+    console.log("Problemas en el servidor...");
+  }
+});
---
+++ caja/ajax/guardaFacturas.php
@@ -0,0 +1,23 @@
+<?php
+  require_once("../../funciones.php");
+  $factura = $_POST['factura'];
+  $cliente = $_POST['cliente'];
+  $nombre = $_POST['nombre'];
+  $estatus = $_POST['estatus'];
+  $fecha = $_POST['fecha'];
+  $descuento = $_POST['descuento'];
+  $importe = $_POST['importe'];
+  $vendedor = $_POST['vendedor'];
+  $contador = $_POST['contador'];
+  $arreglo = array();
+  $arreglo[0] = "Carga Exitosa";
+  $base = conexion_local();
+  for ($i=1; $i < $contador ; $i++){
+    $consulta = "INSERT INTO CARGAS(CLAVE, CLIENTE, NOMBRE, ESTATUS, FECHA, DESCUENTO, IMPORTE, VENDEDOR)
+                        VALUES(?,?,?,?,?,?,?,?)";
+    $resultado = $base->prepare($consulta);
+    $resultado->execute(array($factura[$i], $cliente[$i], $nombre[$i], $estatus[$i], $fecha[$i], $descuento[$i], $importe[$i], $vendedor[$i]));
+  }
+  $resultado->closeCursor();
+  echo json_encode($arreglo);
+?>
